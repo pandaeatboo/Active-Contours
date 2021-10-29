a2e9978009261296b4a30b17681f004269358e69
EricQ <ericqibamboo@gmail.com>
2021-10-29 15:39:06 -0400

    computeA and distPoints done

--- Snakes2D/functions/functions.py
+++ Snakes2D/functions/functions.py
@@ -8,10 +8,22 @@ def computeA(n,alpha,beta):
     d1 = -2*np.ones(n)
     d2 = np.ones(n-1)
     A2 = np.diag(d1) + np.diag(d2,1) + np.diag(d2,-1)
-    A2[1,n] = 1
-    A2[n,1] = 1
+    A2[0,n-1] = 1
+    A2[n-1,0] = 1
     
-    A4 = A2 ** 2
+    A4 = A2 @ A2
     
     return -alpha*A2 + beta*A4
+
+
+def distPoints(V):
     
+    V = np.append(V,V[0,:][np.newaxis],axis=0)
+    
+    difference_squared =  np.square(V[0:-1,:] - V[1:,:])
+    distance = np.sqrt(np.sum(difference_squared,axis=1))[np.newaxis]
+    
+    return distance.T
+
+def interpSnake2(G,V):
+    pass
